9146f9c447d0f95c97dff0665811744198cdab44
Zarul Zubir <zarul.izham@gmail.com>
2023-06-22 06:54:50 +0800

    Add soft deletes on bank model

--- src/Models/Bank.php
+++ src/Models/Bank.php
@@ -4,9 +4,12 @@ namespace ZarulIzham\DuitNowPayment\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Bank extends Model
 {
+    use SoftDeletes;
+    
     protected $table = 'duitnow_banks';
 
     protected $fillable = [
